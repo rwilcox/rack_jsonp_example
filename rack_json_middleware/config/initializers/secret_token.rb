e0240a62780a2fc30d83133f5c3ea29b2bff70c9
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-04-19 22:16:02 -0400

    initial commit of Rails proect

---
+++ rack_json_middleware/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RackJsonMiddleware::Application.config.secret_token = '1fa82d504cd111e9931f846923b39ee15946e5acb64cd0e7731a0752e347a1e5cbe07249530caddfa70ee25c34cfd19ca4ab4a18ba95d911c8c98ba7bdd1f33a'
